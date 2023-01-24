ad1a659085f71133d0d8936e6da33e54d4979d52
sunillangan <58847457+sunillangan@users.noreply.github.com>
2023-01-24 20:04:30 +0800

    updated for sg

--- main.tf
+++ main.tf
@@ -3,16 +3,41 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "b" {
-  bucket = "sunillangangeeta"
-  acl    = "private"
+resource "aws_security_group" "allow_tls" {
+  name = "shah-security_group"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = var.vpn_ip
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "allow_tls"
   }
 }
 
-variable "region" {
-  
-}
